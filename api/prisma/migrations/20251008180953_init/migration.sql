2e749b11beec904a38b78464f49973b85b56b074
Cyriel89 <anthony.carrer@hotmail.fr>
2025-10-08 20:16:14 +0200

    chore: scaffold monorepo (infra, api, service, web) chore(infra): add Postgres + Adminer on 5433/8080 feat(api): init Nest app + Prisma setup (Transaction model)

---
+++ api/prisma/migrations/20251008180953_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "currency" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
