bf2b60b630887f7fb21c185cbf120f3c8297a516
Cyriel89 <anthony.carrer@hotmail.fr>
2025-10-12 16:14:24 +0200

    chore(api): basic hardening (helmet, cors, rate limit)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PresentationModule } from './presentation/PresentationModule';
+import { ThrottlerModule } from '@nestjs/throttler/dist/throttler.module';
+import { ThrottlerGuard } from '@nestjs/throttler/dist/throttler.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [PresentationModule],
+  imports: [PresentationModule, ThrottlerModule.forRoot({
+    throttlers: [
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ],
+  })],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
